c132435ead86e67e84c9aea5ba7aca861251041b
Farhan Habib <f.habibca@gmail.com>
2022-02-02 11:57:37 -0800

    Adding Getters and Setters for Item Class

--- src/item.js
+++ src/item.js
@@ -6,13 +6,39 @@ class Item {
 	constructor(name = "") {
 		if (!name) name = "";
 
-		this.name = name;
-		this.stock = 0;
-		this.price = 0.0;
+		this.#name = name;
+		this.#stock = 0;
+		this.#price = 0.0;
 	}
 	// Add Appropriate getters and setters
-}
 
+	getName() {
+		return this.#name;
+	}
+	setName(name) {
+		if (name != null) name = "";
+		this.#name = name;
+	}
+
+	setStock(stock) {
+		this.#stock = stock;
+	}
+	getStock(stock) {
+		return this.#stock;
+	}
+
+	setPrice(price) {
+		if (!(price >= 0)) throw "Price can not be negative";
+		this.#price = price;
+	}
+	getPrice() {
+		return this.#price;
+	}
+
+	DEBUG() {
+		return this.getName() + " : " + this.getStock() + " : " + this.getPrice();
+	}
+}
 module.exports = {
-	item,
+	Item,
 };
