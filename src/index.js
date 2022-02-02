0297135d9190d5ded9a6a1f7672f38306dfa0ec3
Farhan Habib <f.habibca@gmail.com>
2022-02-02 11:58:05 -0800

    Reading data from file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
 const { BSTree } = require("./BST.js");
+const { Item } = require("./item.js");
+const { ConversionUtil } = require("./Utils/Conversionutil.js");
 
 /**
  * dataArray is an array containing strings of JSON objects
@@ -8,34 +10,32 @@ const { BSTree } = require("./BST.js");
  * Example:
  * {"name":"Shoes of Negligible Looks","stock":-7,"cost":"$52.81"}
  */
-// const dataArray = fs
-// 	.readFileSync("./inventory.txt", "utf8")
-// 	.toString()
-// 	.split("\n");
+const dataArray = fs
+	.readFileSync("./inventory.txt", "utf8")
+	.toString()
+	.split("\n");
 
-// for (const element of dataArray) {
-// 	try {
-// 		jsonArray.push(JSON.parse(element));
-// 		//put into both BSTs
-// 	} catch (e) {
-// 		/* */
-// 	}
-// }
-
-let x = new BSTree((a, b) => {
-	// < 0 if a < b
-	// = 0 if a == b
-	// > 0 if a > b
-	if (a < b) return -1;
-	if (a == b) return 0;
-	if (a > b) return 1;
+let NameSortBSt = new BSTree((itemA, itemB) => {
+	if (itemA.getName().localeCompare(itemB.getName()) === 0) {
+		return 0;
+	} else if (itemA.getName().localeCompare(itemB.getName()) > 0) {
+		return 1;
+	} else {
+		return -1;
+	}
 });
 
-let arr = [1, -10, 3, 2, 4];
+for (let element of dataArray) {
+	try {
+		element = JSON.parse(element);
+		let ItemObject = new Item(element.name);
+		ItemObject.setStock(element.stock);
+		ItemObject.setPrice(ConversionUtil.dollarToNumber(element.cost));
 
-for (let index = 0; index < arr.length; index++) {
-	x.add(arr[index]);
+		//put into both BSTs
+		NameSortBSt.add(ItemObject);
+	} catch (e) {
+		console.log(e);
+	}
 }
-console.log(x.inOrder());
-x.remove(1);
-console.log(x.inOrder());
+console.log(NameSortBSt.inOrder().map((m) => m.getName()));
